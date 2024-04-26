838cd8492bb33fdc7d600caa2b70d53e9df84bcf
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-26 14:15:23 +0530

    fload and integers statements.

---
+++ chapter_2_variables/P04_numbers.py
@@ -0,0 +1,15 @@
+
+"""
+Integers :
+    + , - , * , / , // , ** 
+    Supports order of opertions however you can change the order using parathesis .
+
+Float :
+    Number with decimal point . 
+
+
+"""
+
+# Integers
+
+print(55+2)
