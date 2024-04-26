29dfe8f489b70be9d866bc3769a63d89c5c25b26
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-26 14:35:03 +0530

    completed chapter 2

--- chapter_2_variables/P04_numbers.py
+++ chapter_2_variables/P04_numbers.py
@@ -7,9 +7,24 @@ Integers :
 Float :
     Number with decimal point . 
 
+Under Score in Numbers :
+    When you're woring the long numbers you can use _ to group digits.
+    nums = 14_000_000_000
+    print(nums) # 14000000000
+
+Constants 
+    Python does not have built in type for constants . However as good practise variable name all in caps is considered as constant.
+    MAX_NUMBER = 5000
 
 """
 
 # Integers
-
+age = 14_000_000_000
 print(55+2)
+
+print(age)
+
+x,y,z = 10,20,30
+print(x,y,z)
+
+import this
