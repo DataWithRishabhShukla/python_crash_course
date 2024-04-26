6a95718301c48ec221fe2ccbae99b35c502ef0b3
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-26 11:53:08 +0530

    Added strings part

--- chapter_2_variables/P01_Hello_world.py
+++ chapter_2_variables/P01_Hello_world.py
@@ -5,8 +5,23 @@ print(message)
 message = 'Second Message !!'
 print(message)
 
+print(mesage)
+
 
 """
 1. Syntax Highlighintng - Python iterprepter will scan line by line then apply different color coding to the fucntion / variables / text /loops.
 2. Python will keep track of latest value of the variables. 
+3. Variable names can not start with number . It can't have space in it.
+4. Variables are often described as boxes you can store your value in . However programaticlly variables are labels assigned to the values.
+"""
+sample_string  =  'Python sample strigs !!'
+sample_string2 = "Pythn Sample strings 2 !!"
+"""
+Strings :
+1. A string is series of characters . Anything inside quotes is cosidered strings in python . You can use both [ ' ' or " "].
+2.
+3.
+4. 
+5. 
+
 """
