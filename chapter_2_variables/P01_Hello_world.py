0e4feb848800517ea0823d6848a4e7b697233501
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-26 11:36:53 +0530

    chapter 2 added messages

---
+++ chapter_2_variables/P01_Hello_world.py
@@ -0,0 +1,12 @@
+
+message = 'Hello Python Interpreter !!'
+print(message)
+
+message = 'Second Message !!'
+print(message)
+
+
+"""
+1. Syntax Highlighintng - Python iterprepter will scan line by line then apply different color coding to the fucntion / variables / text /loops.
+2. Python will keep track of latest value of the variables. 
+"""
