ec663de1c29fcac908d5b6a7b4df12c84207dcad
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-26 14:14:43 +0530

    Adding numbers file

---
+++ chapter_2_variables/P03_exercises.py
@@ -0,0 +1,32 @@
+
+# String Exeercise 
+
+# First exercise
+name = 'eric'
+print(f' Hello {name} , would you like to learn some python today ?')
+
+# Second exercise
+name = 'eric matthew'
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+
+# Third exercise
+print('Swami Vivekananda once said , " The greatest sin is to think yourself weak." ')
+
+author = 'Swami Vivekananda'
+quote  = 'The greatest sin is to think yourself weak.'
+print(f'{author} once said , " {quote} "')
+
+# Fouth Exercise
+person_name ='\t Rishabh Shukla \t'
+print(person_name)
+print(person_name.rstrip())
+print(person_name.lstrip())
+print(person_name.strip())
+
+# Final Exercise
+
+file_name = 'ip_addresses.txt'
+print(file_name.removesuffix('.txt'))
--- error_encountered.md
+++ error_encountered.md
@@ -1,5 +1,7 @@
 
-1. Name Error : while trying to reference a variable which is not defined .
+Name Error : while trying to reference a variable which is not defined .
     name ='rishabh'
     print(full_name)
+
+Syntax Error : Occures when python does not recognizes a section of code as valid python code . Like using apostrophe inside the single quotes.
 
