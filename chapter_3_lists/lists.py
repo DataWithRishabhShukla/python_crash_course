a7575389445723f7bd8d603299f9fcccb5dd9de1
rishabhshukla <rishabh.shukla8@gmail.com>
2024-04-27 13:08:40 +0530

    List adding removing and delete

--- chapter_3_lists/lists.py
+++ chapter_3_lists/lists.py
@@ -8,7 +8,23 @@ Accessing a list :
     Using a index , starts with 0 []
     You can use the -ve index . -1 represents the last element .
 
+Modifying , Adding and Removing Elements :
+    Modifying -> Same as accessing the element
     
+    Adding 
+        At the end using the append function . motorcycles.append('ducati')
+        At any location we can do using . motorcycles.insert(0,'hero') . This will shift all the elements to the right .
+
+    Remove
+        Using Index : del motorcycles[1] 
+        
+        Using pop method : This will remove the last element form the list 
+            popped_motorcycles = motorcycles.pop()
+            We can use index with pop to remove the items from the list.
+        
+        Remove using the value :
+            
+
 """
 import os 
 
@@ -24,3 +40,42 @@ print(bicycles[-1])
 
 message = f'My first bicycle was {bicycles[0].title()}'
 print(message)
+
+# Modifying
+
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+motorcycles[0] = 'bajaj'
+print(motorcycles)
+
+# Adding 
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+
+motorcycles.insert(0,'hero')
+print(motorcycles)
+
+
+# Removing Element
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+
+# pop without index 
+os.system('clear')
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+
+# pop with index 
+os.system('clear')
+motorcycles = ['honda','yamaha','suzuki']
+
+first_motorcycle = motorcycles.pop(1)
+print(f'First motorcycle which I owned was: {first_motorcycle.title()}')
+
+# Removing using value 
