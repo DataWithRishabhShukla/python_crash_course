5a4dec992f14aada28900f648c0a71e89b7bb317
rishabhshukla <rishabh.shukla8@gmail.com>
2024-05-13 13:09:41 +0530

    Updated the code to add the path of name_function module

--- Chapter_11_Testing_Your_Code/test_name_function.py
+++ Chapter_11_Testing_Your_Code/test_name_function.py
@@ -1,8 +1,18 @@
+import sys
+sys.path.insert(0,"/Users/rishabhshukla/git_projects/python_crash_course/Chapter_11_Testing_Your_Code/")
 
 from name_function import get_formatted_name
+
+# def get_formatted_name(first, last):
+#     """Generate a neatly formatted full name."""
+#     full_name = f"{first} {last}"
+#     return full_name.title()
 
 def test_first_last_name():
     """Function to test the get_formatted_name """
 
     formatted_name = get_formatted_name('janis', 'joplin')
     assert formatted_name == 'Janis Joplin'
+    # print(formatted_name)
+
+# test_first_last_name()
