6bf6cf8d055481bc540f6eb15829df3f6bc06d1a
rishabhshukla <rishabh.shukla8@gmail.com>
2024-05-13 15:00:33 +0530

    Added more unittest cases

--- Chapter_11_Testing_Your_Code/test_name_function.py
+++ Chapter_11_Testing_Your_Code/test_name_function.py
@@ -1,18 +1,35 @@
-import sys
+"""
+(base) rishabhshukla@Rishabhs-MacBook-Air Chapter_11_Testing_Your_Code % pytest
+===================================== test session starts =====================================
+platform darwin -- Python 3.8.8, pytest-6.2.3, py-1.10.0, pluggy-0.13.1
+rootdir: /Users/rishabhshukla/git_projects/python_crash_course/Chapter_11_Testing_Your_Code
+plugins: anyio-2.2.0, Faker-25.1.0
+collected 1 item                                                                              
+
+test_name_function.py .                                                                 [100%]
+
+====================================== 1 passed in 0.05s ======================================
+(base) rishabhshukla@Rishabhs-MacBook-Air Chapter_11_Testing_Your_Code % 
+
+"""
+
+import sys, os
+
+os.system('clear')
+# Add the absolute path of the directory containing the module to the Python path
 sys.path.insert(0,"/Users/rishabhshukla/git_projects/python_crash_course/Chapter_11_Testing_Your_Code/")
 
 from name_function import get_formatted_name
 
-# def get_formatted_name(first, last):
-#     """Generate a neatly formatted full name."""
-#     full_name = f"{first} {last}"
-#     return full_name.title()
-
 def test_first_last_name():
     """Function to test the get_formatted_name """
 
-    formatted_name = get_formatted_name('janis', 'joplin')
-    assert formatted_name == 'Janis Joplin'
-    # print(formatted_name)
+    formatted_name = get_formatted_name('rishabh', 'shukla')
+    assert formatted_name == 'Rishabh Shukla'
 
-# test_first_last_name()
+def test_first_middle_last_name():
+    """Function to test the get_formatted_name """
+
+    formatted_name = get_formatted_name('rishabh','shukla' ,'kumar')
+    assert formatted_name == 'Rishabh Kumar Shukla'
+
