3cb654ce11e9e2844547efb8468523657aab9526
rshuk3 <rishabh.shukla@nike.com>
2024-06-11 13:53:12 +0530

    added vitualenv

---
+++ python_tricks/M08_Python_Productivity/virtual_env.py
@@ -0,0 +1,25 @@
+"""
+    $ which pip3
+        - /usr/bin/pip3
+
+To create a vitual enviroment
+    $ python3 -m venv ./venv
+        - This is will create a virtual env in the current path 
+        - You can do $ ls -lt ./venv to check current content of file 
+
+To activate the venv 
+    $ source ./venv/bin/activate
+    
+    - To check the current pip location 
+    $ which pip3
+        - /Users/rshuk3/git_personal/python_crash_course/venv/bin/pip3
+    $ % which python
+        - /Users/rshuk3/git_personal/python_crash_course/venv/bin/python
+
+To list all the pip package 
+    $ pip list 
+
+To Deactiavte 
+    - deactivate 
+
+"""
